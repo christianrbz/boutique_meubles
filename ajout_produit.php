36ee2f7f9494cbbcf972cac3d53ed32b45e7b8f2
christianrbz <christian.reubrez@gmail.com>
2022-11-17 15:02:03 +0100

    Première Initialisation

--- ajout_produit.php
+++ ajout_produit.php
@@ -1,1 +1,9 @@
 <?php 
+
+/**
+ * 4 status de fichier sur git
+ *  - untracked => les fichiers créé mais non suivies par git
+ *  - tracked => fichier déjà suivi par git mais non envoyé vers le repository
+ *  - staged => les fichiers suivi, enregistré mais non envoyé vers le repository
+ *  - commited => tous les fichiers envoyés et non modifiés de mon projet 
+ */
